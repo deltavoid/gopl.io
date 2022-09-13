f6756f2adacc7412c100672ad5714f938a159207
zqy <850074816@qq.com>
2022-09-13 18:09:37 +0800

    update dup2

--- ch1/dup2/main.go
+++ ch1/dup2/main.go
@@ -15,7 +15,9 @@ import (
 )
 
 func main() {
+
 	counts := make(map[string]int)
+	
 	files := os.Args[1:]
 	if len(files) == 0 {
 		countLines(os.Stdin, counts)
@@ -30,18 +32,23 @@ func main() {
 			f.Close()
 		}
 	}
+	
 	for line, n := range counts {
 		if n > 1 {
 			fmt.Printf("%d\t%s\n", n, line)
 		}
 	}
+
 }
 
 func countLines(f *os.File, counts map[string]int) {
+
 	input := bufio.NewScanner(f)
+	
 	for input.Scan() {
 		counts[input.Text()]++
 	}
+	
 	// NOTE: ignoring potential errors from input.Err()
 }
 
