403b3cb21dd6e30883a7e9ff65541d737af8cc5e
zqy <850074816@qq.com>
2022-09-13 20:05:15 +0800

    comment fetchall

--- ch1/fetchall/main.go
+++ ch1/fetchall/main.go
@@ -17,33 +17,66 @@ import (
 )
 
 func main() {
+
+	fmt.Println("main: 1")
 	start := time.Now()
+	
 	ch := make(chan string)
+	
+    fmt.Println("main: 2")
 	for _, url := range os.Args[1:] {
+
+
+	    fmt.Println("main: 4")
 		go fetch(url, ch) // start a goroutine
+	
+		fmt.Println("main: 5")
 	}
+	
+	fmt.Println("main: 6")
 	for range os.Args[1:] {
+	
+		fmt.Println("main: 7")
 		fmt.Println(<-ch) // receive from channel ch
+	
+		fmt.Println("main: 8")
 	}
+	
+	fmt.Println("main: 9")
 	fmt.Printf("%.2fs elapsed\n", time.Since(start).Seconds())
+
+    fmt.Println("main: 10")
 }
 
 func fetch(url string, ch chan<- string) {
+	
+	fmt.Println("fetch: 1")
 	start := time.Now()
+
+	fmt.Println("fetch: 2")
 	resp, err := http.Get(url)
+	
+	fmt.Println("fetch: 3")
 	if err != nil {
 		ch <- fmt.Sprint(err) // send to channel ch
 		return
 	}
 
+	fmt.Println("fetch: 4")
 	nbytes, err := io.Copy(ioutil.Discard, resp.Body)
 	resp.Body.Close() // don't leak resources
 	if err != nil {
 		ch <- fmt.Sprintf("while reading %s: %v", url, err)
 		return
 	}
+	
+	fmt.Println("fetch: 5")
 	secs := time.Since(start).Seconds()
+	
+	fmt.Println("fetch: 6")
 	ch <- fmt.Sprintf("%.2fs  %7d  %s", secs, nbytes, url)
+	
+	fmt.Println("fetch: 7")
 }
 
 //!-
