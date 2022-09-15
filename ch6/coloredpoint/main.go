7c8e5acb969bf1555b23e808cc8bbbb94be3d42c
ZhangQianyu <qingxu@lccomputing.com>
2022-09-15 18:51:22 +0800

    update coloredpoing

--- ch6/coloredpoint/main.go
+++ ch6/coloredpoint/main.go
@@ -36,14 +36,18 @@ func (p *Point) ScaleBy(factor float64) {
 
 func main() {
 	//!+main
+
 	red := color.RGBA{255, 0, 0, 255}
 	blue := color.RGBA{0, 0, 255, 255}
 	var p = ColoredPoint{Point{1, 1}, red}
 	var q = ColoredPoint{Point{5, 4}, blue}
+
 	fmt.Println(p.Distance(q.Point)) // "5"
+
 	p.ScaleBy(2)
 	q.ScaleBy(2)
 	fmt.Println(p.Distance(q.Point)) // "10"
+
 	//!-main
 }
 
